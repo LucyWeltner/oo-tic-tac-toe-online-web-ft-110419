41a4e00672c8943e8583c35a7a7823ba662dc061
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-12 04:44:04 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -81,10 +81,12 @@ class TicTacToe
     end
     all_players = [x_positions, o_positions]
     WIN_COMBINATIONS.each do |win_state|
-      if all_players.find_all{|index| array1.included_in?(win_state)}
-        p win_state 
-        p array1
-        return win_state
+      all_players.each do |array1|
+        winning_indices = array1.find_all{|index| array1.include?(win_state)}
+        p winning_indices
+        if winning_indices.length == 3
+          return win_state
+        end
       end
     end
     return nil
